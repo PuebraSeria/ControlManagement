c11eacc5559b4eff4d069678d8055403a621c5b0
Brayan <Brayan@NICOLE>
2016-10-04 09:47:43 -0600

    Cambios en la estructrua

--- CGA.DataAccess/My Project/AssemblyInfo.vb
+++ CGA.DataAccess/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("GCA.DataAccess.TRAN")> 
+<Assembly: AssemblyTitle("CGA.DataAccess")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("GCA.DataAccess.TRAN")> 
+<Assembly: AssemblyProduct("CGA.DataAccess")> 
 <Assembly: AssemblyCopyright("Copyright ©  2016")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("7d4dd2b9-207b-49f6-a1f8-d24b56ea0ea9")> 
+<Assembly: Guid("196bf85b-2e9a-4cff-bd05-4da85c4beb12")> 
 
 ' Version information for an assembly consists of the following four values:
 '
--- CGA.DataAccess/My Project/Settings.Designer.vb
+++ CGA.DataAccess/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.GCA.DataAccess.TRAN.My.MySettings
+        Friend ReadOnly Property Settings() As Global.CGA.DataAccess.My.MySettings
             Get
-                Return Global.GCA.DataAccess.TRAN.My.MySettings.Default
+                Return Global.CGA.DataAccess.My.MySettings.Default
             End Get
         End Property
     End Module
