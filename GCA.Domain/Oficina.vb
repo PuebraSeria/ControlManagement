c4842f215791ea09f6a8ce1f025b45149c98ffd3
Yunen Ramos Ramírez <yunen.ramos@ucr.ac.cr>
2016-10-21 16:54:27 -0600

    Creación de atributos
    Creación de atributos

--- GCA.Domain/Oficina.vb
+++ GCA.Domain/Oficina.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Oficina
-
+  'Declaración de variables
+  Private tc_CodigoValue As String
+  Private tc_NombreValue As String
+  
+  'Método constructor sin parámetros
+  Public Sub New()
+    Me.tc_CodigoValue = ""
+    Me.tc_NombreValue = ""
+  End Sub
+  
+  'Método constructor con parámetros
+  Public Sub New(codigo As String, nombre As String)
+    Me.tc_CodigoValue = codigo
+    Me.tc_NombreValue = nombre
+  End Sub
+  
+  ' Métodos propiedad Código
+  Public Property Codigo() As String
+    Get
+      Return Me.tc_CodigoValue
+    End Get
+    
+    Set
+      Me.tc_CodigoValue = Value
+    End Set
+  End Property
+  
+  ' Métodos propiedad Nombre
+  Public Property Codigo() As String
+    Get
+      Return Me.tc_NombreValue
+    End Get
+    
+    Set
+      Me.tc_NombreValue = Value
+    End Set
+  End Property
 End Class
