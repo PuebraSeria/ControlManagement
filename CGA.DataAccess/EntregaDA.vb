b5c22c22d442a39a3c1dcbe364bd75f28f03994f
Michael <michael.melendezm@gmail.com>
2016-10-21 21:45:52 -0600

    Se agregaron las funciones de Control, se crearon las clases Entrega, EntregaDA y EntregaBusiness con sus respectivas funciones

---
+++ CGA.DataAccess/EntregaDA.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Data.SqlClient
+Imports GCA.Domain
+
+
+Public Class EntregaDA
+
+
+    Private connectionString As String
+
+    Public Sub New(connectionString As String)
+        Me.connectionString = connectionString
+    End Sub
+
+
+    Public Function crearEntrega(entrega As Entrega) As Integer
+
+        Dim conn As New SqlConnection(Me.connectionString)
+
+        Dim sqlProcedimientoAlmacenado As String = "PA_Realizar_Entrega"
+        Dim cmdControl As New SqlCommand(sqlProcedimientoAlmacenado, conn)
+
+        cmdControl.CommandType = CommandType.StoredProcedure
+
+        cmdControl.Parameters.Add(New SqlParameter("@codigoC", entrega.Codigo_Control))
+        cmdControl.Parameters.Add(New SqlParameter("@codigoO", entrega.Codigo_Oficina))
+        cmdControl.Parameters.Add(New SqlParameter("@adjunto", entrega.Adjunto))
+
+        cmdControl.Connection.Open()
+        Dim respuesta As Integer = cmdControl.ExecuteNonQuery()
+        cmdControl.Connection.Close()
+
+
+        Return respuesta
+
+    End Function
+
+End Class
