08bb821d089ed00a84a976cb34da6896f6ca41c0
Yunen Ramos Ramírez <yunen.ramos@ucr.ac.cr>
2016-11-08 15:23:32 -0600

    Se agregó una opción más al Select de obtener los controles de una oficina

--- CGA.DataAccess/OficinaDA.vb
+++ CGA.DataAccess/OficinaDA.vb
@@ -185,7 +185,7 @@ Public Class OficinaDA
         Dim sqlConn As New SqlConnection(Me.connection)
 
         Dim query As String = "select TC_Codigo_DocControl, TC_Nombre_DocControl, TN_Periocidad_DocControl, TF_FechaInicio_DocControl,
-                                TF_FechaFinal_DocControl from TDocControl inner join TOfn_X_DocCtrl on
+                                TF_FechaFinal_DocControl,TF_FechaAsigna_Ofn_X_DocCtrl from TDocControl inner join TOfn_X_DocCtrl on
                                 TDocControl.TC_Codigo_DocControl = TOfn_X_DocCtrl.TC_CodDocControl_Ofn_X_DocCtrl
                                 where TOfn_X_DocCtrl.TC_CodOficina_Ofn_X_DocCtrl = " + codOficina
 
